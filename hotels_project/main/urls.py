ba5112944ce382f87198aa66d66d4962bda64d9a
Tais <taisiya.star@gmail.com>
2024-02-17 23:20:58 +0000

    Add hotel browsing

--- hotels_project/main/urls.py
+++ hotels_project/main/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import browse_hotels
 
 urlpatterns = [
-    path('', admin.site.urls)
+    path('api/hotels/', browse_hotels, name='browse_hotels'),
+    path('', admin.site.urls),
 ]
--- hotels_project/main/views.py
+++ hotels_project/main/views.py
@@ -1,5 +1,23 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Hotel
 
-# Create your views here.
 
+def browse_hotels(request):
+    city_id = request.GET.get('city_id')
+    from_id = request.GET.get('from_id')
+    limit = request.GET.get('limit')
+
+    hotels = Hotel.objects.all()
+
+    if city_id:
+        hotels = hotels.filter(city_id=city_id)
+    if from_id:
+        hotels = hotels.filter(id__gt=from_id)
+    if limit:
+        hotels = hotels[:int(limit)]
+
+    data = list(hotels.values('id', 'name', 'address', 'phone', 'city_id'))
+
+    return JsonResponse(data, safe=False)
